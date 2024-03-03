e8d2797aae3b6b55a29e31838a80a402c2218784
Ryan Paolo Joson <bantora.1011@gmail.com>
2024-03-03 22:55:01 +0800

    feat: typing game

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import wordsGenerator from "./util/wordsGenerator";
+
+const words = ref(wordsGenerator());
+
+const handleInput = (e) => {
+  const foundValue = words.value.find((d) => d == e.target.value);
+
+  if (foundValue) {
+    words.value = words.value.filter((d) => d !== foundValue);
+    e.target.value = "";
+  }
+};
+</script>
 
 <template>
   <div>Typing Game</div>
+  <!-- Typing Options -->
+  <div><input @input="handleInput" /></div>
+  <div class="">{{ words.join(" ") }}</div>
 </template>
 
 <style scoped></style>
---
+++ src/util/wordsGenerator.js
@@ -0,0 +1,12 @@
+import { faker } from "@faker-js/faker";
+
+const wordsGenerator = (max = 20) => {
+  let words = [];
+  for (let i = 0; i <= max; i++) {
+    words.push(faker.word.sample({ length: { min: 5, max: 7 } }));
+  }
+
+  return words;
+};
+
+export default wordsGenerator;
