1950444559c96e472994f1f172354ffc7c9fb1aa
Ryan Paolo Joson <bantora.1011@gmail.com>
2024-03-04 01:11:51 +0800

    feat: highlight correct words

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,34 @@
+<template>
+  <div>Typing Game</div>
+  <!-- Typing Options -->
+  <div>
+    <input @input="handleInput" />
+    <select name="max" @change="handleChange">
+      <option value="10">10</option>
+      <option value="15">15</option>
+      <option value="20">20</option>
+    </select>
+  </div>
+  <ul>
+    <li v-for="word in words" :class="checkWord(word, typedword)">
+      <ul>
+        <li v-for="letter in word">
+          {{ letter }}
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
+
 <script setup>
 import { ref } from "vue";
 import wordsGenerator from "./util/wordsGenerator";
 
-const words = ref(wordsGenerator());
+const maxWords = ref(10);
+
+const typedword = ref("");
+
+const words = ref(wordsGenerator(maxWords.value));
 
 const handleInput = (e) => {
   const foundValue = words.value.find((d) => d == e.target.value);
@@ -11,14 +37,34 @@ const handleInput = (e) => {
     words.value = words.value.filter((d) => d !== foundValue);
     e.target.value = "";
   }
+
+  typedword.value = e.target.value;
+};
+
+const handleChange = (e) => {
+  maxWords.value = +e.target.value;
+  words.value = wordsGenerator(+e.target.value);
+};
+
+const checkWord = (w, c = null) => {
+  console.log(w.startsWith(c), w, c);
+  return w.startsWith(c) ? "correct" : "wrong";
 };
 </script>
 
-<template>
-  <div>Typing Game</div>
-  <!-- Typing Options -->
-  <div><input @input="handleInput" /></div>
-  <div class="">{{ words.join(" ") }}</div>
-</template>
+<style scoped>
+ul {
+  list-style-type: none;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
 
-<style scoped></style>
+.correct {
+  color: rgb(219, 219, 219);
+}
+
+.wrong {
+  color: rgb(116, 116, 116);
+}
+</style>
--- src/util/wordsGenerator.js
+++ src/util/wordsGenerator.js
@@ -1,6 +1,6 @@
 import { faker } from "@faker-js/faker";
 
-const wordsGenerator = (max = 20) => {
+const wordsGenerator = (max) => {
   let words = [];
   for (let i = 0; i <= max; i++) {
     words.push(faker.word.sample({ length: { min: 5, max: 7 } }));
