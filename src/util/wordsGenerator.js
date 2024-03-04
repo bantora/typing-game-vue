ab7ce97351e00f014a11a31df25cf2f7f4694186
Ryan Paolo Joson <bantora.1011@gmail.com>
2024-03-05 00:08:40 +0800

    fix: max words length

--- src/util/wordsGenerator.js
+++ src/util/wordsGenerator.js
@@ -2,7 +2,7 @@ import { faker } from "@faker-js/faker";
 
 const wordsGenerator = (max) => {
   let words = [];
-  while (words.length != 20) {
+  while (words.length != max) {
     const newWord = faker.word.sample({ length: { min: 5, max: 7 } });
 
     if (!words.find((v) => v === newWord)) words.push(newWord);
